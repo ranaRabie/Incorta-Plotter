d8e14c8c10ce11d0786612ef8c26bbe28e281a7a
Rana Rabie <rana.rabie@itworx.com>
2021-10-28 20:51:27 +0200

    Creating Home Container

---
+++ src/Containers/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Chart from '../Components/Chart';
+import List from '../Components/List';
+import Form from '../Components/Form';
+
+function Home () {
+    return (
+        <div>
+            <List />
+            <Form />
+            <Chart />
+        </div>
+    );
+    
+      
+};
+    
+export default Home;
