7555a9e4b3a7cf6dc19cf863214249a886666fc5
Rana Rabie <rana.rabie@itworx.com>
2021-10-28 20:49:15 +0200

    Adding Chart Component

---
+++ src/Components/Chart/index.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { Line } from 'react-chartjs-2';
+
+
+
+const Chart = () => {
+
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+
+    const data = {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      datasets: [
+        {
+          label: "First dataset",
+          data: [33, 53, 85, 41, 44, 65],
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)"
+        },
+        {
+          label: "Second dataset",
+          data: [33, 25, 35, 51, 54, 76],
+          fill: false,
+          borderColor: "#742774"
+        }
+      ]
+    };
+
+    return(
+      <div>
+      <Line data={data} options={options} />
+      </div>
+    );
+
+  
+};
+
+export default Chart;
