c13320ffd6d6ef5c8d707fdbefa4464b6aeaf151
Rana Rabie <rana.rabie@itworx.com>
2021-10-28 20:52:26 +0200

    Creating My Context for Global Variables and Fn()s

---
+++ src/MyContext.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// const Context = React.createContext({
+//     currentMeasure: 'mmmmmmmmmm',
+//     currentDimension: 'ddddddddd',
+//     getItem: (type, name) => {
+//         console.log('clicked');
+        
+//     },
+// });
+
+const Context = React.createContext();
+
+export default Context;
