2893187849c32925e9ef98e1c0c65f9c65ba1d87
Rana Rabie <rana.rabie@itworx.com>
2021-10-28 21:02:21 +0200

    Removing Comments

--- src/MyContext.js
+++ src/MyContext.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-
-// const Context = React.createContext({
-//     currentMeasure: 'mmmmmmmmmm',
-//     currentDimension: 'ddddddddd',
-//     getItem: (type, name) => {
-//         console.log('clicked');
-        
-//     },
-// });
-
 const Context = React.createContext();
 
 export default Context;
