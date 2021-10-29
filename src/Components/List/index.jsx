2226c7d20a4ceac52b75d925373353001208f8ca
Rana Rabie <rana.rabie@itworx.com>
2021-10-29 21:36:14 +0200

    Seperating Measures and Dimesions List

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -21,13 +21,28 @@ const List = () => {
     return (
       <div className="list-wrapper">
         <h3>List</h3>
-        <p>click on an item to select it</p>
+        <p className="mb-1 border-0">click on an item to select Dimension</p>
         <ul>
-          {List?.map((item) => ( // List Loop
+          {List?.map((item) => {
+            return item.function === "dimension" ?
               <li key={item.name} >
-                <span onClick={() => context.getItems(item.function, item.name)} className={`${context.currentDimension === item.name || context.currentMeasure.includes(item.name) ? "disabled" : ""}`}>{item.name}</span>
+                <span onClick={() => context.getItems(item.function, item.name)} className={`${context.currentDimension === item.name ? "disabled" : ""}`}>{item.name}</span>
               </li>
-            ))
+              : ''
+            }
+            )
+          }
+        </ul>
+        <p className="mb-1 mt-3">click on an item to select Measure</p>
+        <ul>
+          {List?.map((item) => {
+            return item.function === "measure" ?
+              <li key={item.name} >
+                <span onClick={() => context.getItems(item.function, item.name)} className={`${context.currentMeasure.includes(item.name) ? "disabled" : ""}`}>{item.name}</span>
+              </li>
+              : ''
+            }
+            )
           }
         </ul>
        
